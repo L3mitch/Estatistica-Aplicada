6c1ae0c4881af2d00c931491be2ab7501cd03b88
EliasEnns <elias0enns@gmail.com>
2024-05-14 22:27:27 -0300

    Corrigido tratamento da requisicao

--- source.py
+++ source.py
@@ -20,16 +20,19 @@ requisicoes = [[69, 2, "kauan_nunes", 0, 0],
 def ler_requisicoes():
     url = "http://localhost:3000/requisicoes"
     response = requests.get(url)
-    return response.json()
+    treated_response = [response["id"], response["tipo"], response["nome"], 0, 0]
+    return treated_response
 
 def enviar_resposta(requisicao):
     url = "http://localhost:3000/requisicoes"
-    data = {requisicao}
+    data = {"id": requisicao[0], "nome": requisicao[2], "sucesso": requisicao[4]}
+
     response = requests.post(url, data=data)
     return response.json()
 
-
 while True:
+    # Ler requisições
+    requisicoes += ler_requisicoes()
     # Tratamento da requisição
     for requisicao in requisicoes:
         if requisicao[3] == 1:
