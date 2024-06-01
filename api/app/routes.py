eced919fc6f345edca2cdb4884b6d55d0fb2700b
Juliano Silva <juliano.sil6@gmail.com>
2024-06-01 14:43:51 -0300

    Ajustes leiturae gravação de dados dos usuários

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,10 +1,41 @@
 import os 
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app import app
 import base64
+import sqlite3
 from detector import * # Isso precisa mudar, não está importando do local correto
 
-import csv
+db_file = "pesquisas.db"
+
+try:
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS pesquisas (
+        id INTEGER PRIMARY KEY,
+        dir_name TEXT NULL,
+        name TEXT NULL,
+        age TEXT NULL,
+        email TEXT NULL,
+        sex TEXT NULL,
+        photo_1 TEXT NULL,
+        empresa TEXT NULL,
+        q1_01 TEXT NULL,
+        q1_02 TEXT NULL,
+        q1_03 TEXT NULL,
+        q1_04 TEXT NULL,
+        q1_05 TEXT NULL,
+        q2_01 TEXT NULL,
+        q2_02 TEXT NULL,
+        q2_03 TEXT NULL,
+        q2_04 TEXT NULL,
+        q2_05 TEXT NULL
+    )
+    """
+    cursor.execute(create_table_query)
+except sqlite3.Error as e:
+    print(f"Error: {e}")
 
 
 @app.route('/')
@@ -23,7 +54,6 @@ def leitura():
 @app.route('/resposta_inicio', methods=['POST'])
 def salva_resposta_inicio():
     if request.method == 'POST':
-        filename, fields = config_data()
 
         file_name = request.form['nome'].lower().replace(' ', '')
         dir_user = 'api/training/' + file_name
@@ -37,35 +67,7 @@ def salva_resposta_inicio():
         with open(dir_user + '/' + f2_titulo, "wb") as fh:
             fh.write(threatPhoto(request))
 
-        rows = [
-            [
-                file_name,
-                request.form['nome'],
-                request.form['idade'],
-                request.form['email'],
-                request.form['sexo'],
-                f1_titulo,
-                'f2_titulo',
-                request.form['q1_01'],
-                request.form['q1_02'],
-                request.form['q1_03'],
-                request.form['q1_04'],
-                request.form['q1_05'],
-                request.form['q1_06'],
-                request.form['q1_07'],
-                '',
-                '',
-                '',
-                '',
-                ''
-            ]
-        ]
-
-        with open(filename, 'w') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(fields)
-            csvwriter.writerows(rows)
-
+        adicionar_pesquisa(file_name, f1_titulo, request)
         adicionar_pessoa(request)
         return {
             'success': True,
@@ -74,66 +76,22 @@ def salva_resposta_inicio():
 
 @app.route('/resposta_fim', methods=['GET'])
 def resposta_fim():
-    user = request.args.get('user', default = '1')
-    filename, fields = config_data()
+    user = request.args.get('user', default = 'null')
+    usuario = busca_pesquisa(user)
 
-    with open(filename, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
-        for row in csv_reader:
-            if row["dir_name"] == user:
-                fields = row
-            line_count += 1
+    if (not usuario):
+        return redirect("/", code=302)
 
-    return render_template('resposta_fim.html', title='Resposta final', u=fields)
+    return render_template('resposta_fim.html', title='Resposta final', u=usuario)
 
 @app.route('/resposta_fim', methods=['POST'])
 def salva_resposta_fim():
     if request.method == 'POST':
-        filename, fields = config_data()
-
-        with open(filename, mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            line_count = 0
-            for row in csv_reader:
-                if row["dir_name"] == request.form['dir_name']:
-                    fields = row
-                line_count += 1
-                
-        rows = [
-            [
-                fields['dir_name'],
-                fields['name'],
-                fields['age'],
-                fields['email'],
-                fields['sex'],
-                'f1_titulo',
-                'f2_titulo',
-                fields['q1_01'],
-                fields['q1_02'],
-                fields['q1_03'],
-                fields['q1_04'],
-                fields['q1_05'],
-                fields['q1_06'],
-                fields['q1_07'],
-                request.form['q2_02'],
-                request.form['q2_03'],
-                request.form['q2_04'],
-                request.form['q2_05'],
-                request.form['q2_07']
-            ]
-        ]
-
-        with open(filename, 'w') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(fields)
-            csvwriter.writerows(rows)
-
+        atualizar_pesquisa(request)
         return {
             'success': True,
             'message': 'salva_resposta_inicio cadastradas'
         }
-
 
 
 @app.route('/leitura_facial', methods=['POST'])
@@ -152,6 +110,11 @@ def leitura_facial():
         recognize_face(file_name, model="hog")
     }
 
+@app.route('/graficos', methods=['GET'])
+def graficos():
+    refresh = request.args.get('refresh', default = '1')
+
+    return render_template('x_graficos.html', title='Resposta final', r=busca_grafico(), refresh=refresh)
 
 def adicionar_pessoa(request):
     nome = request.form['nome']
@@ -159,12 +122,93 @@ def adicionar_pessoa(request):
         encode_new_face(nome, model="hog")
     }
 
-def config_data():
-    filename = "pesquisas.csv"
-    fields = ['dir_name', 'name', 'age', 'email', 'sex', 'photo_1', 'photo_2', 'q1_01', 'q1_02', 'q1_03', 'q1_04', 'q1_05', 'q1_06', 'q1_07', 'q2_02', 'q2_03', 'q2_04', 'q2_05', 'q2_07']
-
-    return filename, fields
-
 def threatPhoto(request):
     userPhoto = request.form['user_photo'].replace('data:image/png;base64,', '');
     return base64.b64decode(userPhoto)
+
+def busca_pesquisa(pesquisa):
+    try:
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+
+        sql = "SELECT * FROM pesquisas WHERE id = ?"
+        cursor.execute(sql, (pesquisa))
+
+        return cursor.fetchone()
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+
+def busca_grafico():
+    try:
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+
+        sql = """SELECT 
+                    SUM(case when (q1_01 = 'S') THEN 1 else 0 end) as q1_01_s,
+                    SUM(case when (q1_02 = 'S') THEN 1 else 0 end) as q1_02_s,
+                    SUM(case when (q1_03 = 'S') THEN 1 else 0 end) as q1_03_s,
+                    SUM(case when (q1_04 = 'S') THEN 1 else 0 end) as q1_04_s,
+                    SUM(case when (q1_05 = 'S') THEN 1 else 0 end) as q1_05_s,
+                    SUM(case when (q1_01 = 'N') THEN 1 else 0 end) as q1_01_n,
+                    SUM(case when (q1_02 = 'N') THEN 1 else 0 end) as q1_02_n,
+                    SUM(case when (q1_03 = 'N') THEN 1 else 0 end) as q1_03_n,
+                    SUM(case when (q1_04 = 'N') THEN 1 else 0 end) as q1_04_n,
+                    SUM(case when (q1_05 = 'N') THEN 1 else 0 end) as q1_05_n,
+                    SUM(case when (q2_01 = 'S') THEN 1 else 0 end) as q2_01_s,
+                    SUM(case when (q2_02 = 'S') THEN 1 else 0 end) as q2_02_s,
+                    SUM(case when (q2_03 = 'S') THEN 1 else 0 end) as q2_03_s,
+                    SUM(case when (q2_04 = 'S') THEN 1 else 0 end) as q2_04_s,
+                    SUM(case when (q2_05 = 'S') THEN 1 else 0 end) as q2_05_s,
+                    SUM(case when (q2_01 = 'N') THEN 1 else 0 end) as q2_01_n,
+                    SUM(case when (q2_02 = 'N') THEN 1 else 0 end) as q2_02_n,
+                    SUM(case when (q2_03 = 'N') THEN 1 else 0 end) as q2_03_n,
+                    SUM(case when (q2_04 = 'N') THEN 1 else 0 end) as q2_04_n,
+                    SUM(case when (q2_05 = 'N') THEN 1 else 0 end) as q2_05_n,
+                    COUNT(*) AS total
+                FROM pesquisas p"""
+        cursor.execute(sql)
+
+        return cursor.fetchone()
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+
+def adicionar_pesquisa(file_name, f1_titulo, request):
+    try:
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+
+        sql = ''' INSERT INTO pesquisas(
+                dir_name, name, age, email, sex, photo_1, empresa,
+                q1_01, q1_02, q1_03, q1_04, q1_05,
+                q2_01, q2_02, q2_03, q2_04, q2_05
+            )
+            VALUES(
+                ?,?,?,?,?,?,?,
+                ?,?,?,?,?,
+                ?,?,?,?,?
+            ) '''
+        pesquisa_data = (
+            file_name, request.form['nome'], request.form['idade'], request.form['email'], request.form['sexo'], f1_titulo, request.form['empresa'],
+            request.form['q1_01'], request.form['q1_02'], request.form['q1_03'], request.form['q1_04'], request.form['q1_05'], 
+            ' ', ' ', ' ', ' ', ' '
+        )
+
+        cursor.execute(sql, pesquisa_data)
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+
+def atualizar_pesquisa(request):
+    try:
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+
+        sql = ''' UPDATE pesquisas SET q2_01 = ?, q2_02 = ?, q2_03 = ?, q2_04 = ?, q2_05 = ? WHERE id = ? '''
+        pesquisa_data = (
+            request.form['q2_01'], request.form['q2_02'], request.form['q2_03'], request.form['q2_04'], request.form['q2_05'], request.form['id']
+        )
+
+        cursor.execute(sql, pesquisa_data)
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
